1b07e8a3c4444968c8d1036930019262a9731543
mobious952004 <pranjalgupta@ce.du.ac.in>
2025-09-27 09:40:08 +0530

    global search user chat bubble

--- frontend/src/Chat/components/PrevChat.jsx
+++ frontend/src/Chat/components/PrevChat.jsx
@@ -1,107 +1,109 @@
 import { EllipsisVerticalIcon } from "lucide-react";
+import { PinIcon } from "lucide-react";
 import { useEffect } from "react";
 import { useRef } from "react";
 // import "flowbite";
 import { useState } from "react";
 
-export default function PrevChat({msg}) {
+export default function PrevChat({ msg }) {
 
-  const menueRef=useRef(null)
-  const [menueopen,setmenueopen]=useState(false)
-  
+  const menueRef = useRef(null)
+  const [menueopen, setmenueopen] = useState(false)
 
-  useEffect(()=>{
 
-    const outsideclick=(e)=>
-      {
-        if(menueRef.current && !menueRef.current.contains(e.target)){setmenueopen(false)}
-      }
-      
-      document.addEventListener("mousedown", outsideclick);
-      return () => document.removeEventListener("mousedown", outsideclick);
-  },[menueRef])
+  useEffect(() => {
+
+    const outsideclick = (e) => {
+      if (menueRef.current && !menueRef.current.contains(e.target)) { setmenueopen(false) }
+    }
+
+    document.addEventListener("mousedown", outsideclick);
+    return () => document.removeEventListener("mousedown", outsideclick);
+  }, [menueRef])
 
   return (
-    <div className={`flex items-start gap-2.5 group relative  ${msg.belongstouser?"self-end flex-row-reverse":"self-start flex-row"}`}ref={menueRef}>
+    <div className={`flex my-2 items-start gap-2.5 group relative  ${msg.belongstouser ? "self-end flex-row-reverse" : "self-start flex-row"}`} ref={menueRef}>
       <img className="w-8 h-8 rounded-full" src="/react.svg" alt="Jese image" />
       <div className="flex flex-col gap-1 w-full max-w-[320px]">
-        <div className={`flex items-center space-x-2 ltr:space-x-reverse ${msg.belongstouser ? "justify-end space-x-reverse" : "space-x-2"}`}>
-          <span className="text-sm font-semibold text-gray-900 dark:text-white">
+        <div className={` flex items-center ${msg.belongstouser ? "justify-start flex-row-reverse space-x-reverse gap-4" : "gap-2"}`}>
+          <span className="text-sm font-semibold text-emerald-300 dark:text-emerald-100">
             {msg.from.username}
           </span>
           <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-            {msg.time }&nbsp;{msg.date}
+            {msg.time}
           </span>
         </div>
-        <div className={`flex flex-col flex-wrap leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl  ${msg.belongstouser?"bg-gray-600 rounded-ss-xl":"dark:bg-green-600"} `}>
+        <div className={`flex flex-col flex-wrap leading-1.5 p-4 border-gray-200 bg-gray-100   ${msg.belongstouser ? "bg-gray-600 rounded-s-xl rounded-ee-xl" : "dark:bg-green-600 rounded-e-xl rounded-es-xl"} `}>
           <p className="text-sm font-normal  text-gray-900 dark:text-white whitespace-pre-wrap break-all">
             {msg.content}
           </p>
         </div>
-        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-          {msg.status}
-        </span>
+        <div className={` flex items-center ${msg.belongstouser ? "justify-start flex-row-reverse space-x-reverse gap-4" : "gap-2"}`}>
+          <span className="text-sm font-normal text-gray-500 dark:text-gray-400 ">
+            {msg.status}
+          </span>
+          <span className="text-sm font-normal text-gray-500 dark:text-gray-400 ">
+            {msg.date}
+          </span>
+
+        </div>
       </div>
       <button
 
-        className=  {`inline-flex self-center items-center  p-2 text-sm font-medium text-center text-gray-900 rounded-lg focus:ring-0 focus:outline-none dark:text-white opacity-0 group-hover:opacity-100`}
+        className={`  inline-flex self-center items-center  p-2 text-sm font-medium text-center text-gray-900 rounded-lg focus:ring-0 focus:outline-none dark:text-white opacity-0 group-hover:opacity-100`}
         type="button"
-        onClick={()=>setmenueopen(!menueopen)}
+        onClick={() => setmenueopen(!menueopen)}
 
       >
         <EllipsisVerticalIcon />
       </button>
 
-      {menueopen && 
-      <div
-      className="absolute right-0 bottom-0 mt-2  z-10  bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-40 dark:bg-gray-700 dark:divide-gray-600"
-      >
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+      {menueopen &&
+        <div
+          className={`absolute ${msg.belongstouser?"right-40":"left-0" }   bottom-15 mt-0  z-10  bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-40 dark:bg-gray-700 dark:divide-gray-600`}
         >
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Reply
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Forward
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          >
+            <li>
+              <button
+                type="button" className="w-full block px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
-              Copy
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                Reply
+              </button>
+            </li>
+            <li>
+              <button
+               type="button" className="w-full block px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
-              Report
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                Forward
+              </button>
+            </li>
+            <li>
+              <button
+               type="button" className="w-full block px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
-              Delete
-            </a>
-          </li>
-        </ul>
-      </div>
-            }
+                Copy
+              </button>
+            </li>
+            <li>
+              <button
+                type="button" className="w-full block px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                pin
+              </button>
+
+            </li>
+            <li>
+               <button
+                type="button" className="w-full block px-4 py-2 text-start hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+      }
     </div>
   );
 }
